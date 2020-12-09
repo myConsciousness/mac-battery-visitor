990712f3bb2154e24e8f2d8c15f93b5e4fcd69c5
Kato Shinya <kato.shinya.dev@gmail.com>
2020-12-09 15:41:56 +0900

    fixed

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     """,
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: Apache License 2.0',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
