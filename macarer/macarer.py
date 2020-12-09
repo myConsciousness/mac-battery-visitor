e308ec8547a10d9be03007f49f398acad63b30b3
Kato Shinya <kato.shinya.dev@gmail.com>
2020-12-09 09:11:52 +0900

    fixed structure

--- macarer/macarer.py
+++ macarer/macarer.py
@@ -1,38 +1,64 @@
-from subprocess import Popen, PIPE
+from os import getenv
+import re
 import sys
+import subprocess
 import requests
 
-cmd = '/usr/sbin/ioreg -l | /usr/bin/grep Capacity'
+BATTERY_UPPER_LIMIT = 0.79
+BATTERY_LOWER_LIMIT = 0.31
 
-output = Popen(cmd.split(), stdout=PIPE).communicate()[0]
-output_str = str(output, encoding='utf-8')
+IOREG_CAPACITY_COMMAND = '/usr/sbin/ioreg -l | /usr/bin/grep Capacity'
+IOREG_CHARGE_COMMAND = '/usr/sbin/ioreg -l | /usr/bin/grep ExternalChargeCapable'
 
-battery_info = dict(
-    [(kw.strip().strip('"'), vw.strip())
-     for kw, vw
-     in [line.split("=", 1)
-         for line
-         in output_str.split('\n') if line.find('=') > 0
-         ]
-     ]
-)
+LINE_NOTIFY_API = 'https://notify-api.line.me/api/notify'
+LINE_NOTIFY_TOKEN = str(getenv('LINE_NOTIFY_TOKEN_FOR_MACARER'))
 
-# mAh
-current_capa = int(battery_info["CurrentCapacity"])
-# mAh
-max_capa = int(battery_info["MaxCapacity"])
 
-charge_ratio = int((current_capa/max_capa) * 100)
+def examine_battery():
 
-url = 'https://notify-api.line.me/api/notify'
-token = 'xxxxxxx'
-headers = {'Authorization': 'Bearer ' + token}
+    max_capacity, current_capacity = get_battery_capacities()
 
-if charge_ratio >= 90:
-    message = ''
-    payload = {"message": message}
-    r = requests.post(url, headers=headers, params=payload)
-elif charge_ratio <= 20:
-    message = ''
-    payload = {"message": message}
-    r = requests.post(url, headers=headers, params=payload)
+    if (int(max_capacity) > 0 and int(current_capacity) > 0):
+
+        rate = float(current_capacity) / float(max_capacity)
+
+        if is_charging_battery():
+            if rate >= BATTERY_UPPER_LIMIT:
+                send_notification('')
+        else:
+            if rate <= BATTERY_LOWER_LIMIT:
+                send_notification('')
+
+
+def get_battery_capacities():
+
+    capacities = subprocess.check_output(IOREG_CAPACITY_COMMAND, shell=True).decode(
+        'utf-8').strip().split('\n')
+
+    max_capacity = 0
+    current_capacity = 0
+
+    for st in capacities:
+        ar = st.split('=')
+        key = re.sub('^(.*?")', '', ar[0]).replace('" ', '')
+
+        if (key == 'MaxCapacity'):
+            max_capacity = ar[1].strip()
+        elif key == 'CurrentCapacity':
+            current_capacity = ar[1].strip()
+
+    return max_capacity, current_capacity
+
+
+def is_charging_battery():
+    return True if subprocess.check_output(IOREG_CHARGE_COMMAND, shell=True).decode(
+        'utf-8').strip().split('=')[1].strip() == 'Yes' else False
+
+
+def send_notification(message):
+    requests.post(LINE_NOTIFY_API, headers={
+        'Authorization': 'Bearer ' + LINE_NOTIFY_TOKEN}, params={'message': message})
+
+
+if __name__ == '__main__':
+    examine_battery()
