da80efb55ecc4a184f2ff4cef04996d86c9cde8f
Kato Shinya <kato.shinya.dev@gmail.com>
2020-12-09 11:07:24 +0900

    update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='macarer',
     version='1.0.0',
-    description='The script created to optimize the battery charging state of the Macbook.',
+    description='The script created to optimize the battery charging state of the MacBook.',
     author='Kato Shinya',
     author_email='kato.shinya.dev@gmail.com',
     url='https://github.com/myConsciousness/macarer',
